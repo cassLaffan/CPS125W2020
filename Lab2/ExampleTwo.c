47b1db55aaa0b30d1d1ed9db38a45c644e8b072d
Cassie <poisonberryvines@gmail.com>
2020-01-23 18:01:11 -0500

    Adding a text file, wrapping up for the night.

---
+++ Lab2/ExampleTwo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*This example will explore file I/O*/
+/*This program will read from one file and write to another.*/
+
+int main(){
+    FILE *fileOne, *fileTwo;
+
+
+    return 0;
+}
