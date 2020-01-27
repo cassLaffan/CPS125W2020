4b7b012ea7f61ffb1410235bc0f1e93b7be25f3a
cassLaffan <poisonberryvines@gmail.com>
2020-01-26 19:15:24 -0500

    Adding a more pertinent example for Lab 2.

--- Lab2/ExampleTwo.c
+++ Lab2/ExampleTwo.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
-/*This example will explore calculating the surface area of a sphere*/
+/*This example will explore calculating the surface area and volume of a icosahedron (20 sided dice)*/
+/*This will take user input as its side length parameter*/
 
-void main(void){
+float surfaceArea(float a){
 
-    printf("%f", 3/4);
+    return (5 * sqrt(3) * pow(a, 2));
+}
+
+float volume(float a){
+
+    return (5.0/12.0) * (3 + sqrt(5)) * pow(a, 3);
+}
+
+int main(){
+
+    printf("What is the edge length of your icosahedron?\n");
+
+    float edgeLength;
+
+    scanf("%f", &edgeLength);
+
+    printf("The surface area of your icosahedron is: %f\n", surfaceArea(edgeLength));
+
+    printf("The volume of your icosahedron is: %f\n", volume(edgeLength));
+
+    return 0;
 
 }
