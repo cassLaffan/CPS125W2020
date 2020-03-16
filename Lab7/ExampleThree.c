46189485096467b1612d9a8f327743098ed14eb8
cassLaffan <poisonberryvines@gmail.com>
2020-03-16 12:38:55 -0400

    Reading from a file and using an array

---
+++ Lab7/ExampleThree.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*This example expands on an example from lab 5.*/
+
+int main(){
+
+    FILE *fp; //remember, we discussed pointers last lab
+    int arr[100]; //We know there are 100 integers in my associated text file.
+    int num = 0;
+    char* filename = "ints.txt"; 
+    fp = fopen(filename, "r"); //the r means it's for reading!
+
+    //Let's check to see if the file actually opened.
+    //If not, we'll exit the program with an early return statement
+
+    if(fp == NULL){
+        printf("It's file broken day, my dudes. %s \n", filename);
+        return(1);
+    }
+
+    //now we read the contents of the file to the terminal
+    //This time I'm using an example of syntax that you guys have used, yourselves
+    while (!feof(fp)){
+        fscanf(fp, "%d", &arr[num]);
+        num++;
+    }
+
+    //remember to always close your files after you're done reading them
+    fclose(fp);
+
+    for(int i = 0; i < 100; i++){
+        printf("The array at index %d holds the value: %d\n", i, arr[i]);
+    }
+    return(0);
+}
